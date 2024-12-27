8460c1ff45940a1b3bfa246e4dd729897fc67d2c
bai <bai@baturax.com>
2024-12-27 18:47:09 +0300

    bai

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,8 @@
-if status is-interactive
+#source /usr/share/cachyos-fish-config/cachyos-config.fish
+function fish_greeting
+    fastfetch -c examples/8.jsonc
 end
 
-alias ls='eza --color=always --color-scale-mode=gradient --icons=always --hyperlink -a --group-directories-first -s=Extension'
-alias cat='bat'
+alias gpush="git add . && git commit -m 'bai' && git push"
 alias ch='chmod +x'
-alias gsa="git submodule add"
-alias gpush="git add . && git commit -m 'do' && git push"
-
+alias ls='eza --color=always --color-scale-mode=gradient --icons=always --hyperlink -a --group-directories-first -s=Extension'
