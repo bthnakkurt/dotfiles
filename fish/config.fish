98fefe342a028b17c0bebdb42a52503cee7971cf
b <b>
2025-01-01 21:06:58 +0300

    bai

--- fish/config.fish
+++ fish/config.fish
@@ -5,4 +5,8 @@ end
 
 alias gpush="git add . && git commit -m 'bai' && git push"
 alias ch='chmod +x'
+alias sudo='sudo -E'
 alias ls='eza --color=always --color-scale-mode=gradient --icons=always --hyperlink -a --group-directories-first -s=Extension'
+alias cat='bat'
+alias cr='cargo run'
+alias c='clear'
